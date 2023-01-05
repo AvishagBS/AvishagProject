0dc37fe40ce5d6ff6f62fe7dde7ebb3dd3144af4
Avishag BS <a0583228516@gmail.com>
2023-01-05 14:48:13 +0200

    the context change

---
+++ AvishagProject.Context/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using AvishagProject.Repositories;
+using AvishagProject.Repositories.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AvishagProject.Context
+{
+    public class DataContext: DbContext, IContext
+    {
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Permission> Permissions { get; set; }
+        public DbSet<Claim> Claims { get; set; }
+
+    }
+}
